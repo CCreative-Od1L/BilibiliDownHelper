908b52026c2381f1b20d9bc30ef8f074368e2894
pleGGen <crelative@163.com>
2024-01-31 21:50:21 +0800

    20240131-控制窗口出现位置试做

---
+++ src/BvDownkr/src/ViewModels/SearchPageVM.cs
@@ -0,0 +1,45 @@
+﻿using BvDownkr.src.Utils;
+using BvDownkr.src.Views;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace BvDownkr.src.ViewModels
+{
+    public class SearchPageVM {
+        public static ICommand OpenWindowTest {
+            get {
+                return new ReplyCommand<Button>(
+                    (Button? btn) => {
+                        double top_p = 0d;
+                        double left_p = 0d;
+                        // * 屏幕缩放率
+                        Graphics graphics = Graphics.FromHwnd(IntPtr.Zero);
+                        var ratio = (graphics.DpiX * 1.041666667) / 100;
+
+                        if (btn != null) {
+                            System.Windows.Point point = btn.PointToScreen(new System.Windows.Point(0d, 0d));
+                            top_p = point.Y / ratio + btn.ActualHeight;
+                            left_p = point.X / ratio + btn.ActualWidth;
+                        }
+                        
+                        var userWindow = new UserWindow {
+                            Top = top_p,
+                            Left = left_p,
+                        };
+                        userWindow.Left -= userWindow.Width / 2;
+
+                        userWindow.ShowDialog();
+                    }, 
+                    true);
+            }
+            
+        }
+    }
+}
