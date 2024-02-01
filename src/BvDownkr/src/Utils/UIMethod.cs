5cfdd0acb210d8e47b00816caec86ca344730fe0
pleGGen <crelative@163.com>
2024-02-01 15:23:31 +0800

    20240201-复杂的东西不要用wpf的popup

---
+++ src/BvDownkr/src/Utils/UIMethod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BvDownkr.src.Utils {
+    internal class UIMethod {
+        public static Point GetMousePosition() {
+            return Mouse.GetPosition(Application.Current.MainWindow);
+        }
+        public static bool IsMouseInUI<T>(T uiElement, Point mousePos) where T : FrameworkElement {
+            // * 主窗口 为坐标系
+            Window window = Window.GetWindow(uiElement);
+            var uiElementPoint = uiElement.TransformToAncestor(window).Transform(new Point(0d, 0d));
+            if (mousePos.X < uiElementPoint.X || 
+                mousePos.X > uiElementPoint.X + uiElement.Width ||
+                mousePos.Y < uiElementPoint.Y || 
+                mousePos.Y > uiElementPoint.Y + uiElement.Height) { return false; }
+            else { return true; }
+        }
+    }
+}
--- src/BvDownkr/src/Views/SearchPage.xaml.cs
+++ src/BvDownkr/src/Views/SearchPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BvDownkr.src.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
